1f66ded9280ae737a4838366454d6701a4f38e34
Jake <jakereal2004@gmail.com>
2024-08-21 21:35:15 +0100

    Add leader key macros, system combos, new punction

---
+++ vault35rp/keymaps/sherman/emails.h
@@ -0,0 +1,4 @@
+#define WORK_EMAIL "email"
+#define PERSONAL_EMAIL "email"
+#define UNI_EMAIL "email"
+
--- vault35rp/keymaps/sherman/keymap.c
+++ vault35rp/keymaps/sherman/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "emails.h"
 
 enum layer_names {
     _BASE,
@@ -63,14 +64,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SS_QAZ_PASTA:
             if (record->event.pressed) {
-                SEND_STRING("You guys always kill me because it's like "
+                SEND_STRING_DELAY(
+                    "You guys always kill me because it's like "
                     "you come up with a funny idea for a keyboard meme "
                     "product and then someone spends actual effort "
                     "mocking it up or making a CAD model and then someone "
                     "decides to actually make it and you spend real "
                     "money on it because nobody had the sense to just " 
                     "say \"this is actually stupid\" somewhere along "
-                    "the way."
+                    "the way.",
+                    1
                 );
             }
             break;
@@ -84,25 +87,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,      KC_Y,            KC_U,          KC_I,          KC_O,          KC_P,    KC_QUOT,
         LGUI_T(KC_A),  LALT_T(KC_S),  LSFT_T(KC_D),  LCTL_T(KC_F),  KC_G,      KC_H,            RCTL_T(KC_J),  RSFT_T(KC_K),  RALT_T(KC_L),  RGUI_T(KC_SCLN),
         KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,      KC_N,            KC_M,          KC_COMM,       KC_DOT,        KC_SLSH,
-        MO(_FUN),                     MO(_NAV),                     KC_SPC,    KC_BSPC,                        MO(_PUN),                     MO(_NUM)
+        MO(_NAV),                     MO(_NUM),                     KC_SPC,    KC_BSPC,                        MO(_PUN),                     MO(_FUN)
     ),
     [_NAV] = LAYOUT_wkl(
-        KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,      KC_Y,            KC_U,          KC_I,          KC_O,          KC_P,    KC_TRNS,
+        KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,      KC_Y,            KC_U,          KC_PGUP,       KC_O,          KC_P,    KC_TRNS,
         KC_LGUI,       KC_LALT,       KC_LSFT,       KC_LCTL,       KC_HOME,   KC_LEFT,         KC_DOWN,       KC_UP,         KC_RIGHT,      KC_END,
-        KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,      KC_N,            KC_M,          KC_COMM,       KC_DOT,        KC_SLSH,
+        KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,      KC_N,            KC_PGDN,       KC_COMM,       KC_DOT,        KC_SLSH,
         KC_TRNS,                      KC_TRNS,                      KC_TRNS,   KC_TRNS,                        KC_TRNS,                      KC_TRNS
     ),
     [_PUN] = LAYOUT_wkl(
-        KC_EXLM,       KC_ASTR,       KC_HASH,       KC_MINS,       KC_PLUS,   KC_NO,           KC_AT,         KC_DOLLAR,     KC_PERC,       KC_CIRC, KC_TRNS,
-        KC_PIPE,       KC_AMPR,       KC_LPRN,       KC_RPRN,       KC_DQUO,   KC_QUOT,         KC_LCTL,       KC_LSFT,       KC_LALT,       KC_LGUI,
-        KC_TILD,       KC_DLR,        KC_LBRC,       KC_RBRC,       KC_GRAVE,  KC_LCBR,         KC_RCBR,       KC_BSLS,       KC_NO,         KC_NO,
+        KC_EXLM,       KC_AT,         KC_HASH,       KC_AMPR,       KC_ASTR,   KC_NO,           KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_TRNS,
+        KC_DLR,        KC_PERC,       KC_MINS,       KC_EQL,        KC_PLUS,   KC_CIRC,         KC_LCTL,       KC_LSFT,       KC_LALT,       KC_LGUI,
+        KC_BSLS,       KC_GRV,        KC_TILD,       KC_PIPE,       KC_NO,     KC_NO,           KC_NO,         KC_NO,         KC_NO,         KC_SLSH,
         KC_TRNS,                      KC_TRNS,                      KC_TRNS,   KC_TRNS,                        KC_TRNS,                      KC_TRNS
     ),
     [_NUM] = LAYOUT_wkl(
-        KC_DOT,        KC_7,          KC_8,          KC_9,          KC_MINS,   KC_Y,            KC_U,          KC_I,          KC_O,          KC_P,    KC_TRNS,
-        LGUI_T(KC_1),  LALT_T(KC_2),  LSFT_T(KC_3),  LCTL_T(KC_4),  KC_0,      KC_5,            RCTL_T(KC_6),  RSFT_T(KC_7),  RALT_T(KC_8),  RGUI_T(KC_9),
-        KC_PERC,       KC_4,          KC_5,          KC_6,          KC_EQL,    KC_N,            KC_M,          KC_COMM,       KC_DOT,        KC_TRNS,
-        KC_TRNS,                      KC_TRNS,                      KC_TRNS,   KC_TRNS,                        KC_TRNS,                      KC_TRNS
+        KC_MINS,       KC_7,          KC_8,          KC_9,          KC_PLUS,   KC_NO,           KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_TRNS,
+        LGUI_T(KC_0),  LALT_T(KC_1),  LSFT_T(KC_2),  LCTL_T(KC_3),  KC_4,      KC_5,            RCTL_T(KC_6),  RSFT_T(KC_7),  RALT_T(KC_8),  RGUI_T(KC_9),
+        KC_ASTR,       KC_4,          KC_5,          KC_6,          KC_EQL,    KC_NO,           KC_NO,         KC_TRNS,       KC_TRNS,       KC_TRNS,
+        KC_TRNS,                      KC_TRNS,                      KC_TRNS,   KC_TRNS,                        QK_LEAD,                      KC_TRNS
     ),
     [_FUN] = LAYOUT_wkl(
         KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_F11,    KC_F12,          KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_NO,
@@ -111,9 +114,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,                      KC_TRNS,                      KC_TRNS,   KC_TRNS,                        KC_TRNS,                      KC_TRNS
     ),
     [_KEY] = LAYOUT_wkl(
-        QK_BOOT,       EE_CLR,        KC_E,          KC_R,          KC_T,      KC_Y,            KC_U,          KC_I,          KC_O,          S(C(QK_MAKE)),  QK_MAKE,
-        LGUI_T(KC_A),  LALT_T(KC_S),  LSFT_T(KC_D),  LCTL_T(KC_F),  KC_G,      UG_PREV,         UG_VALD,       UG_VALU,       UG_NEXT,       KC_TRNS,
-        KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,      KC_N,            KC_M,          KC_COMM,       KC_DOT,        KC_NO,
+        QK_BOOT,       KC_NO,         KC_NO,         KC_NO,         KC_NO,     KC_NO,           KC_NO,         KC_BRIU,       KC_NO,         KC_PWR,  KC_SLEP,
+        KC_A,          KC_VOLD,       KC_VOLU,       KC_MNXT,       KC_NO,     UG_PREV,         UG_VALD,       UG_VALU,       UG_NEXT,       KC_NO,
+        KC_Z,          SS_QAZ_PASTA,  EE_CLR,        KC_NO,         KC_NO,     KC_NO,           KC_BRID,       KC_COMM,       KC_DOT,        KC_NO,
         KC_TRNS,                      KC_TRNS,                      UG_TOGG,   KC_TRNS,                        KC_TRNS,                      KC_TRNS
     ),
 };
@@ -123,7 +126,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] = { ENCODER_CCW_CW(KC_UP, KC_DOWN) },
     [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
 #endif
 
@@ -137,18 +140,26 @@ enum combos {
     COMM_DOT_RCBR,
     AS_TAB,
     SPACE_BSPC,
-    QAZ_PASTA
+    QAZ_PASTA,
+    Z_C_COPY,
+    Z_X_CUT,
+    Z_V_PASTE,
+    NUM_PUN_LEADER
 };
 
 const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM fun_num_combo[] = {MO(_FUN), MO(_NUM), COMBO_END};
+const uint16_t PROGMEM fun_num_combo[] = {MO(_NAV), MO(_FUN), COMBO_END};
 const uint16_t PROGMEM p_quot_combo[] = {KC_P, KC_QUOT, COMBO_END};
 const uint16_t PROGMEM o_p_combo[] = {KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM m_comm_combo[] = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM comm_dot_combo[] = {KC_COMM, KC_DOT, COMBO_END};
 const uint16_t PROGMEM as_combo[] = {LGUI_T(KC_A), LALT_T(KC_S), COMBO_END};
 const uint16_t PROGMEM space_bspc_combo[] = {KC_SPC, KC_BSPC, COMBO_END};
-const uint16_t PROGMEM qaz_pasta_combo[] = {QK_BOOT, LGUI_T(KC_A), KC_Z, COMBO_END};
+const uint16_t PROGMEM qaz_pasta_combo[] = {KC_A, KC_Z, COMBO_END};
+const uint16_t PROGMEM z_c_combo[] = {KC_Z, KC_C, COMBO_END};
+const uint16_t PROGMEM z_x_combo[] = {KC_Z, KC_X, COMBO_END};
+const uint16_t PROGMEM z_v_combo[] = {KC_Z, KC_V, COMBO_END};
+const uint16_t PROGMEM leader_combo[] = {MO(_NUM), MO(_PUN), COMBO_END};
 
 combo_t key_combos[] = {
     [QW_ESC] = COMBO(qw_combo, KC_ESC),
@@ -159,7 +170,11 @@ combo_t key_combos[] = {
     [COMM_DOT_RCBR] = COMBO(comm_dot_combo, KC_RCBR),
     [AS_TAB] = COMBO(as_combo, KC_TAB),
     [SPACE_BSPC] = COMBO(space_bspc_combo, KC_ENTER),
-    [QAZ_PASTA] = COMBO(qaz_pasta_combo, SS_QAZ_PASTA)
+    [QAZ_PASTA] = COMBO(qaz_pasta_combo, SS_QAZ_PASTA),
+    [Z_C_COPY] = COMBO(z_c_combo, C(KC_C)),
+    [Z_X_CUT] = COMBO(z_x_combo, C(KC_X)),
+    [Z_V_PASTE] = COMBO(z_v_combo, C(KC_V)),
+    [NUM_PUN_LEADER] = COMBO(leader_combo, QK_LEAD),
 };
 
 bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
@@ -168,6 +183,7 @@ bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode
             if (!layer_state_is(_KEY)) {
                 return false;
             }
+            break;
     }
     return true;
 }
@@ -190,6 +206,12 @@ void vim_write(void) {
 void leader_end_user(void) {
     if (leader_sequence_one_key(KC_W)) {
         vim_write();
+    } else if (leader_sequence_one_key(KC_E)) {
+        SEND_STRING_DELAY(PERSONAL_EMAIL, 1);
+    } else if (leader_sequence_three_keys(KC_W, KC_R, KC_A)) {
+        SEND_STRING_DELAY(WORK_EMAIL, 1);
+    } else if (leader_sequence_three_keys(KC_U, KC_S, KC_W)) {
+        SEND_STRING_DELAY(UNI_EMAIL, 1);
     }
 }
 #endif
